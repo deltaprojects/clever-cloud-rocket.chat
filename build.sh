b60731b676521fa7e0893afa7e586311c8027908
Elias Abacioglu <elias.rabi@gmail.com>
2020-04-12 17:01:55 +0200

    Update variable

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-RC_VERSION="${RC_VERSION:=3.0.12}" # Set to 3.0.12 if not already set to something else.
+RC_VERSION="${RC_VERSION:=3.1.0}" # Set to 3.1.0 if not already set to something else.
 
 curl -fSL "https://releases.rocket.chat/${RC_VERSION}/download" | tar xz
 cd bundle/programs/server
